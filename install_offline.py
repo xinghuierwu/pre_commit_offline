9d4c537eb101145d7748b6db5bb7666980d34a18
xinghuierwu <1430065480@qq.com>
2022-05-10 16:39:21 +0800

    第二次提交

--- install_offline.py
+++ install_offline.py
@@ -5,72 +5,112 @@ import zipfile
 import sqlite3
 from sqlite3 import Error
 import argparse
-parser = argparse.ArgumentParser(
-    description= "Install pre-commit offline"
-)
-parser.add_argument('-p','--python-package-path',dest='package_path',required=True)
-parser.add_argument('-g','--Git-projects-path',dest='git_path',required=True)
-parser.add_argument('-c','--pre-commit-cache-zip-path',dest='cache_path',required=True)
-args = parser.parse_args()
-if not args.package_path and args.git_path and args.cache_path:
-    print("The required parameters are not complete")
-    sys.exit(1)
+cache_dir = os.environ['userprofile']
+def dir_exist(path):
+    '''Check whether the path exists'''
+    if not os.path.exists(path):
+        print(f'The {path} does not exist!')
+        sys.exit(1)
+    print('The path exists')
 
-package_path = args.p
-git_path = args.g
-cache_path = args.c
-if sys.version[:4]!='3.8':
-    raise Exception("Python version error!")
-if not os.path.exists(package_path):
-    raise Exception("The python package path is not exist!")
-if not os.path.exists(git_path):
-    raise Exception("The Git projects path is not exist!")
-if not os.path.exists(cache_path):
-    raise Exception("The pre-commit cache zip path is not exist!")
-if not ('Git\\cmd' in os.environ['PATH']):
-    raise Exception("There is no Git in the environment variables!")
-command = '{}\\Scripts\\pip.exe install pre-commit'.format(sys.executable[:-11])
-PIP_var = '{}\\Downloads\\pyformat'.format(os.environ['userprofile'])
-subprocess.Popen(command,env={'PIP_FIND_LINKS' :  PIP_var,
-                              'PIP_NO_INDEX' : 1 }   )
-os.chdir(git_path)
-command2 = "sys.executable[:-11]\\Scripts\\pre-commit.exe install"
-subp = subprocess.Popen(command2)
-if subp.poll() != 0:
-    raise Exception("The subprocess exit code is not 0!")
-# zip_src源文件夹
-# dst_dir目标文件夹
-def unzip_file(zip_src,dst_dir):
+def IsGitEnv():
+    '''Check whether Git is in the environment variables'''
+    p = subprocess.Popen('where git',
+                         shell=True,
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.STDOUT, )
+    if 'git' not in str(p.communicate()[0]):
+        print("No Git in the environment variables!")
+        sys.exit(-1)
+    print("Git is in the environment variables.")
+    return
+
+def SetEnv(package_path):
+    '''Set the environment variables and run the command to install package'''
+    command = f'{sys.executable} -m pip install pre-commit'
+    sub = subprocess.Popen( command,
+                            shell=True,
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.STDOUT,
+                            env={
+                                'PIP_FIND_LINKS': package_path,
+                                'PIP_NO_INDEX': '1'
+                            })
+    ret = sub.wait()
+    if ret != 0:
+        print('The return code of the subprocess is not 0!')
+        sys.exit(1)
+    return
+
+def RunCmd(command):
+    '''Run the command and check the return code'''
+    sub = subprocess.Popen(command,
+                            shell=True,
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.STDOUT)
+    ret = sub.wait()
+    if ret != 0:
+        print('The return code of the subprocess is not 0!')
+        sys.exit(1)
+
+def unzip_file(zip_src, dst_dir):
+    '''Unzip file to the destination path'''
     r = zipfile.is_zipfile(zip_src)
     if r:
-        fz = zipfile.ZipFile(zip_src,'r')
+        fz = zipfile.ZipFile(zip_src, 'r')
         for file in fz.namelist():
-            fz.extract(file,dst_dir)
+            fz.extract(file, dst_dir)
     else:
-        raise Exception("This is not zip file")
-dest_dir = '{}\\.cache'.format(os.environ['userprofile'])
-unzip_file(cache_path,dest_dir)
-try:
-    db_file = '{}\\.cache\\pre-commit\\db.db'.format(os.environ['userprofile'])
-    conn = sqlite3.connect(db_file)
-    cur = conn.cursor()
-    path1 = '{}\\.cache\\pre-commit\\repo8hglikdx' .format(os.environ['userprofile'])
-    path2 = '{}\\.cache\\pre-commit\\repoemx3pt21' .format(os.environ['userprofile'])
-    path3 = '{}\\.cache\\pre-commit\\repow7w1qbj0' .format(os.environ['userprofile'])
-    sql1 = 'update repos set path = {} where ref = 5.10.1 '.format(path1)
-    sql2 = 'update repos set path = {} where ref = 4.0.1 ' .format(path2)
-    sql3 = 'update repos set path = {} where ref = 22.3.0 '.format(path3)
-    cur.execute(sql1)
-    cur.execute(sql2)
-    cur.execute(sql3)
-    conn.commit()
-    cur.close()
-    conn.close()
-except Error as e:
-    print(e)
-    print("连接失败")
-command3 = "{}\\Scripts\\pre-commit.exe install-hooks".format(sys.executable[:-11])
-sub = subprocess.Popen(command3)
-if sub.poll() != 0:
-    raise Exception("The subprocess exit code is not 0!")
+        print("This is not a zip file")
+        sys.exit(-1)
+    return
+def cli():
+    parser = argparse.ArgumentParser(
+        description="Install pre-commit offline"
+    )
+    parser.add_argument('-p', '--python-package-path', dest='package_path', required=True)
+    parser.add_argument('-g', '--Git-projects-path', dest='git_path', required=True)
+    parser.add_argument('-c', '--pre-commit-cache-zip-path', dest='cache_path', required=True)
+    args = parser.parse_args()
+    if not (args.package_path and args.git_path and args.cache_path):
+        print("The required parameters are not complete")
+        sys.exit(1)
+    package_path = args.p
+    git_path = args.g
+    cache_path = args.c
+    if sys.version[:4] != '3.8':
+        print("Python version error!")
+        sys.exit(1)
+    dir_exist(package_path)
+    dir_exist(git_path)
+    dir_exist(cache_path)
+    IsGitEnv()
+    SetEnv(package_path)
+    install_cmd = f'{sys.executable} -m pre_commit install'
+    RunCmd(install_cmd)
+    dest_dir = f'{cache_dir}\\.cache'
+    unzip_file(cache_path,dest_dir) #zip_src = cache_path
+    try:
+        db_file = 'D:\\pre-commit\\db.db'
+        conn = sqlite3.connect(db_file)
+        cur = conn.cursor()
+        path1 = f'{cache_dir}\\.cache\\pre-commit\\repo8hglikdx'
+        path2 = f'{cache_dir}\\.cache\\pre-commit\\repoemx3pt21'
+        path3 = f'{cache_dir}\\.cache\\pre-commit\\repow7w1qbj0'
+        sql1 = f"update repos set path = '{path1}' where ref = '5.10.1' "
+        sql2 = f"update repos set path = '{path2}' where ref = '4.0.1' "
+        sql3 = f"update repos set path = '{path3}' where ref = '22.3.0' "
+        cur.execute(sql1)
+        cur.execute(sql2)
+        cur.execute(sql3)
+        conn.commit()
+        cur.close()
+        conn.close()
+    except Error as e:
+        print(e)
+        print("Connect false!")
+    hooks_cmd = f'{sys.executable} -m pre_commit install-hooks'
+    RunCmd(hooks_cmd)
+    print('Pre-commit has been installed offline successfully!')
+cli()
 
