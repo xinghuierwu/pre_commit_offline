645ff000df79ce394e804f380bb072586668e586
xinghuierwu <1430065480@qq.com>
2022-05-11 17:35:20 +0800

    xg

--- install_offline.py
+++ install_offline.py
@@ -18,21 +18,18 @@ def dir_exist(path):
     print("The path exists")
 
 
-def run_cmd(command, envs):
-    """Set environment Variables,Run the command and check the return code"""
+def run_cmd(command, **kwargs):
+    """Set environment variables, run the command"""
     sub = subprocess.Popen(
         command,
         shell=True,
         stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT,
-        env=envs,
+        **kwargs,
     )
+    val = sub.communicate()
     ret = sub.wait()
-    if ret != 0:
-        print("error cmd:", command)
-        print("The return code of the subprocess is not 0!")
-        sys.exit(1)
-    return
+    return (ret, val)
 
 
 def unzip_file(zip_src, dst_dir):
@@ -79,15 +76,22 @@ def cli():
     dir_exist(cache_path)
 
     check_git_cmd = "git --version"
-    run_cmd(check_git_cmd, envs)
+
+    if run_cmd(check_git_cmd, env=envs)[0] != 0:
+        print("Environment variables do not contain Git!")
+        sys.exit(1)
 
     pip_install_cmd = f"{sys_path} -m pip install pre-commit"
 
-    run_cmd(pip_install_cmd, envs)
+    if run_cmd(pip_install_cmd, env=envs)[0] != 0:
+        print("pip install failed!")
+        sys.exit(1)
 
     install_cmd = f"{sys_path} -m pre_commit install"
 
-    run_cmd(install_cmd, envs)
+    if run_cmd(install_cmd, env=envs)[0] != 0:
+        print("pre_commit install failed!")
+        sys.exit(1)
 
     dest_dir = f"{cache_dir}\\.cache"
     unzip_file(cache_path, dest_dir)  # zip_src = cache_path
@@ -100,9 +104,9 @@ def cli():
         path1 = f"{cache_dir}\\.cache\\pre-commit\\repo8hglikdx"
         path2 = f"{cache_dir}\\.cache\\pre-commit\\repoemx3pt21"
         path3 = f"{cache_dir}\\.cache\\pre-commit\\repow7w1qbj0"
-        sql1 = f"update repos set path = '{path1}' where ref = '5.10.1' "
-        sql2 = f"update repos set path = '{path2}' where ref = '4.0.1' "
-        sql3 = f"update repos set path = '{path3}' where ref = '22.3.0' "
+        sql1 = f"update repos set path = '{path1}' where repo = 'https://github.com/PyCQA/isort' "
+        sql2 = f"update repos set path = '{path2}' where repo = 'https://github.com/PyCQA/flake8' "
+        sql3 = f"update repos set path = '{path3}' where repo = 'https://github.com/psf/black' "
 
         cur.execute(sql1)
         cur.execute(sql2)
@@ -117,7 +121,9 @@ def cli():
         print("Connect false!")
 
     hooks_cmd = f"{sys_path} -m pre_commit install-hooks"
-    run_cmd(hooks_cmd, envs)
+    if run_cmd(hooks_cmd, env=envs)[0] != 0:
+        print("pre_commit install-hooks failed!")
+        sys.exit(1)
 
     print("Pre-commit has been installed offline successfully!")
 
